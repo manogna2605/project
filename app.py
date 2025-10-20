5f6436a21d9837492842f40dd0f46b0d4bdab3d4
manogna2605 <manognathota5@gmail.com>
2025-10-20 16:11:34 +0530

    requirement

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False, allow_unsafe_werkzeug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
+
 
 
 
